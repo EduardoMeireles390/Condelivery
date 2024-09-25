56952e33e70f24e047568b50d465d7b129c22c4f
Eduardo Meireles <126266667+EduardoMeireles390@users.noreply.github.com>
2024-09-25 19:21:27 -0300

    menu_ajustado

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuList = document.getElementById('menu-list');
     const closeMenuButton = document.getElementById('close-menu');
 
+    // Abrir/fechar menu ao clicar no ícone do menu móvel
     mobileMenu.addEventListener('click', () => {
         const isExpanded = mobileMenu.getAttribute('aria-expanded') === 'true';
         mobileMenu.setAttribute('aria-expanded', !isExpanded);
@@ -10,12 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
         menuList.setAttribute('aria-hidden', isExpanded);
     });
 
+    // Fechar menu ao clicar no botão de fechar
     closeMenuButton.addEventListener('click', () => {
         menuList.classList.remove('show');
         mobileMenu.setAttribute('aria-expanded', 'false');
         menuList.setAttribute('aria-hidden', 'true');
     });
 
+    // Fechar o menu ao clicar fora dele
     document.addEventListener('click', (event) => {
         if (!mobileMenu.contains(event.target) && !menuList.contains(event.target)) {
             menuList.classList.remove('show');
